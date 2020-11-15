0d77294acfa7eae3f321bacecd5dcc15b93f850d
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-11-15 12:51:21 +0200

    update 15.11.2020

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -6,21 +6,25 @@ namespace Task_8
     {
         static void Main(string[] args)
         {
-            MyComplex A = new MyComplex(2, 5);
-            A.OutputComNumb(A);
-            MyComplex B = new MyComplex(7, 8);
+            MyComplex A = new MyComplex("A", 2, 5);
+            A.OutputComNumb();
+            MyComplex B = new MyComplex("B", 7, 8);
+            B.OutputComNumb();
 
-            MyComplex C = new MyComplex();
+            MyComplex C = new MyComplex("C");
             C = A * B;
-            C.OutputComNumb(C);
+            Console.WriteLine("C = A * B");
+            C.OutputComNumb();
 
-            MyComplex D = new MyComplex();
+            MyComplex D = new MyComplex("D");
             D = C * 15;
-            D.OutputComNumb(D);
+            Console.WriteLine("D = C * 15");
+            D.OutputComNumb();
 
-            MyComplex E = new MyComplex();
+            MyComplex E = new MyComplex("E");
             E = 13 * D;
-            E.OutputComNumb(E);
+            Console.WriteLine("E = 13 * D");
+            E.OutputComNumb();
 
 
         }
@@ -42,27 +46,34 @@ namespace Task_8
             set { im = value; }
         }
 
-        public MyComplex(double a = 0, double b = 0 ) { re = a; im = b; }
-
-        public void OutputComNumb(object MyComplex)
+        private string name;
+        public string Name
         {
-            Console.WriteLine($"{MyComplex} = {re}+{im}*i");
-            Console.WriteLine();
+            get { return name; }
+            set { name = value; }
         }
 
+        public MyComplex(string n = " ", double a = 0, double b = 0) { name = n; re = a; im = b; }
+
+        public void OutputComNumb()
+        {
+            Console.WriteLine($"{name} = {re}+{im}*i");
+            Console.WriteLine();
+        }
+        
         public static MyComplex operator *(MyComplex a, MyComplex b)
         {
-            return new MyComplex { re = a.re * b.re, im = a.im * b.im };
+            return new MyComplex { re = a.re * b.re, im = a.im * b.im, name = "Result"};
         }
         public static MyComplex operator *(MyComplex a, double b)
         {
-            return new MyComplex { re = a.re * b, im = a.im * b };
+            return new MyComplex { re = a.re * b, im = a.im * b, name = "Result" };
         }
         public static MyComplex operator *(double b, MyComplex a)
         {
-            return new MyComplex { re = a.re * b, im = a.im * b };
+            return new MyComplex { re = a.re * b, im = a.im * b, name = "Result" };
         }
-
+        
 
     }
 }
